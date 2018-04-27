326deff7fe028786dcfcbc6222e86bbe6f851e1f
geraJuarez <geratrex781@gmail.com>
2018-04-26 21:45:23 -0500

    Practica 15: masks and daemons

---
+++ practicas/practica15/bitfields.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// ^ - XOR
+// | - OR
+// & - AND
+// << - Corrimiento a la izquierda
+// >> - Corrimiento a la derecha 
+
+typedef struct d {
+	// ':' acota el tamanio del tipo de dato en bits
+	unsigned char day:5;
+	unsigned char month:4;
+	unsigned char year;
+} Date;
+
+int main()
+{
+	Date today = {26, 4, 2018};
+	printf("today is %d%d%d \n", today.day, today.month, today.year);
+	//today.day += 10;
+
+	if ((today.day ^ 26) == 0)
+	{
+		printf("Today is 26\n");
+	}
+
+	return 0;
+}
---
+++ practicas/practica15/daemon.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int pid = fork();
+	if (pid == 0)
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			fclose(stdout);
+			fclose(stdin);
+			while(1)
+			{
+			}
+		}
+	}
+	return 0;
+}
